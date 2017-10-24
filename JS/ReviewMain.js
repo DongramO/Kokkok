403b89d039a7164ebd92cd1fa59435643da3c756
dongsudongsu <30032dongsu@gmail.com>
2017-10-24 13:44:02 +0900

    upload all file

---
+++ JS/ReviewMain.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	$('.menu2').on('click', function() {
+
+		if ($(this).hasClass("on")) {
+			$(this).css('background-color', '#ffffff');
+			$('#main2').slideToggle();
+			$(this).removeClass("on");
+		} else {
+			$('#main2').slideToggle();
+			$(this).css('background-color', '#cccccc');
+			$(this).addClass("on");
+		}
+	});
+	
+	$('.chk').change(function(){
+		var id=$(this).attr('id');
+		
+		var parent=$(this).parent('.ckbox').next();
+		if($(this).is(':checked')){
+			parent.removeAttr("disabled");
+		}else{
+			parent.attr("disabled",true);
+		}
+	});
+	
+	$('.search_btn').click(function(){
+		var area=$('.menu2_text').val();
+		area=area.substring(0,area.length-1);
+		
+		var content=$('#search_content').val();
+		var tag=$('#search_tag').val();
+		
+		var url="ReviewList.jsp?area="+area+"&content="+content+"&tag="+tag;
+		location.href=url;
+	});
+});
